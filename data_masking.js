ed3d3eff858c7e224da859aef532f7cdeb3ae4fe
Madhu Madhavan Sridhar <47154773+madhumadhavansridhar@users.noreply.github.com>
2020-03-25 17:41:47 -0700

    Add files via upload
    This js file is for encrypting credentials using md5.
    please make sure to import md5 library in angular before testing this file

---
+++ data_masking.js
@@ -0,0 +1,26 @@
+const md5 = require("md5");
+
+const md5 = require("md5");
+module.exports = {
+  hash(rawPassword, options = {}) {
+    
+    const salt = options.salt ? options.salt : new Date().getTime();
+
+    const rounds = options.rounds ? options.rounds : 10;
+
+    let hashed = md5(rawPassword + salt);
+    for (let i = 0; i <= rounds; i++) {
+      hashed = md5(hashed);
+    }
+    return `${salt}$${rounds}$${hashed}`;
+  },
+  compare(rawPassword, hashedPassword) {
+    try {
+      const [ salt, rounds ] = hashedPassword.split('$');
+      const hashedRawPassword = this.hash(rawPassword, { salt, rounds });
+      return hashedPassword === hashedRawPassword;
+    } catch (error) {
+      throw Error(error.message);
+    }
+  }
+};
