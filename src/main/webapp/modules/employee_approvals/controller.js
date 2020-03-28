7a4e644b26cbb0c459a590b57fcfd342cd397f6e
Michael Kintscher <michael.troop90@gmail.com>
2020-03-28 14:44:39 -0700

    Refactored request approval controller and service based on new API and examples.

--- src/main/webapp/modules/employee_approvals/controller.js
+++ src/main/webapp/modules/employee_approvals/controller.js
@@ -8,4 +8,67 @@ angular.module('Approvals')
             function ($scope, $rootScope, $location, ApprovalsService) {
                 // Do stuff
 
+				// Get pending transactions for approval.
+				$scope.GetPendingTransactions = function () {
+
+					$scope.dataLoading = true;
+					ApprovalsService.GetTransactionsPendingApproval(function (response) {
+
+						console.log(response)
+
+						if (response) {
+							$scope.transactionList = response;
+						}
+
+						$scope.dataLoading = false;
+					})
+				}
+
+				// Get pending account actions for approval.
+				$scope.GetPendingTransactions = function () {
+
+					$scope.dataLoading = true;
+					ApprovalsService.GetAccountPendingApproval(function (response) {
+
+						console.log(response)
+
+						if (response) {
+							$scope.accountList = response;
+						}
+
+						$scope.dataLoading = false;
+					})
+				}
+
+				// Respond to pending transaction by approving or denying.
+				$scope.RespondToPendingTransactions = function () {
+
+					$scope.dataLoading = true;
+					ApprovalsService.RepondToTransactionApproval(id, approve, function (response) {
+
+						console.log(response)
+
+						if (response) {
+							$scope.accountList = response;
+						}
+
+						$scope.dataLoading = false;
+					})
+				}
+
+				// Respond to pending account actions by approving or denying.
+				$scope.RespondToPendingAccounts = function () {
+
+					$scope.dataLoading = true;
+					ApprovalsService.RepondToAccountApproval(id, approve, function (response) {
+
+						console.log(response)
+
+						if (response) {
+							$scope.accountList = response;
+						}
+
+						$scope.dataLoading = false;
+					})
+				}
             }]);
