4a37cd3e3be9eca07b72a6266411c517f92fcb00
sushmita594 <smuthe@asu.edu>
2020-04-02 21:30:43 -0700

    Add files via upload

---
+++ src/main/webapp/modules/AccountRequest/AccountRequestController.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('AccountRequest')
+
+.controller('AccountRequestController',
+    ['$scope', '$window', '$rootScope','$state','AccountRequestService',
+    function ($scope, $window, $rootScope, $state, AccountRequestService) {
+    	$scope.approveErrorMsg = "";
+    	$scope.isApproveSuccess = false;
+    	$scope.declineErrorMsg = "";
+    	$scope.isDeclineSuccess = false;
+        $scope.accountList =  [ {name:'Savings Account', id:0},
+                                 {name:'Checkings Account', id:1},
+                                 {name:'Credit Card Account', id:2}
+                               ];
+    	$scope.account;
+
+
+       $scope.SubmitAccountType = function () {
+               if(!$scope.SelectedAccountId){
+                var msg = "Please select an account type";
+                $window.alert(msg);}
+                else{
+                var accountId = $scope.SelectedAccountId;
+                var accountName;
+                for (var i = 0; i < $scope.accountList.length; i++) {
+                    if ($scope.accountList[i].id == accountId) {
+                        accountName = $scope.accountList[i].name;
+                        break;
+                    }
+                }
+                var message = "Value: " + accountId + " Text: " + accountName + "\n";
+                console.log(message);
+                AccountRequestService.submitAccountType(accountId, function(response) {
+                      		  console.log("controller response");
+                      		  $scope.dataLoading = true;
+                      	  })
+         }}
+
+
+    }]);
+
